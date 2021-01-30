4495276f98668697cf7e613ca01d6208dcf1208d
Team-on <timonsol13@gmail.com>
2021-01-30 22:28:53 +0200

    Cats cards

--- GGJ21/Assets/Scripts/Enums.cs
+++ GGJ21/Assets/Scripts/Enums.cs
@@ -10,11 +10,20 @@ using Random = UnityEngine.Random;
 
 public enum PetType : byte { 
 	None = 0,
-	Cat = 1,
-	Dog = 2,
+	Cat1 = 1,
+	Cat2 = 2,
+	Cat3 = 3,
+	Dog1 = 4,
 }
 
 public enum AccessoryType : byte {
 	None = 0,
-	Collar = 1,
+	chains = 1,
+	collar = 2,
+	eye_path = 3,
+	hat = 4,
+	ribbon = 5,
+	spiked_collar = 6,
+	sunglasses = 7,
+	sweater = 8,
 }
